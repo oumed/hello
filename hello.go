525a6e0aaaa9e9c0d675b090d1e140254f6844ab
oumed <mouedrassi@gmail.com>
2021-10-13 23:15:47 -0400

    ref2

---
+++ hello.go
@@ -0,0 +1,16 @@
+package hello
+
+import (
+	"fmt"
+)
+
+func SayHi()  {
+	fmt.Println("Hi every one ")
+	//os.Create("abc")
+}
+
+
+func SaySalam()  {
+	fmt.Println("Salam every one ")
+	//os.Create("abc")
+}
